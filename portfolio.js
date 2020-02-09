384ee0a84707ec8a50f49dd83e6de6168e24758c
CraigA1987 <craigadam1987@gmail.com>
2020-02-09 21:08:09 +0000

    NavBar fix2
    Nav bar now expands and collapses correctly and icons are displayed accordingly

--- portfolio.js
+++ portfolio.js
@@ -1,11 +1,22 @@
 let clicked = false;
 
-document.querySelector(".nav-expand").addEventListener("click", navExpand);
+const openNavButton = document.querySelector(".nav-expand-button");
+const closeNavButton = document.querySelector(".nav-close-button");
+
+openNavButton.addEventListener("click", navExpand);
+closeNavButton.addEventListener("click", closeNav);
+
+const listButtons = document.querySelectorAll(".nav-link");
+listButtons.forEach(function(btn) {
+  btn.addEventListener("click", closeNav);
+});
 
 function navExpand() {
   if (!clicked) {
     console.log("clicked");
-    document.getElementById("navbar").style.width = "80%";
+    document.getElementById("navbar").style.width = "50%";
+    openNavButton.style.display = "none";
+    closeNavButton.style.display = "flex";
     clicked = true;
   } else {
     closeNav();
@@ -15,4 +26,7 @@ function navExpand() {
 
 function closeNav() {
   document.getElementById("navbar").style.width = "0";
+  closeNavButton.style.display = "none";
+  openNavButton.style.display = "flex";
+  clicked = false;
 }
