73ffeedd9b4b0c33ed5d7540f3fd07d2eab3a766
CraigA1987 <craigadam1987@gmail.com>
2020-02-22 23:46:59 +0000

    Added light / Dark mode
    Added a light and dark mode feature. On loading, JavaScript will look to see what mode the operating system is in, and provide the correct color theme. User can also click on the sun or moon to switch themes manually.

--- portfolio.js
+++ portfolio.js
@@ -77,8 +77,70 @@ function animateOnView() {
 }
 
 // Particle.js landing background
-
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
 particlesJS.load("particles-js", "/assets/particles.json", function() {
   console.log("callback - particles.js config loaded");
 });
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+
+// Dealing with theme change between light and dark
+
+const html = document.getElementsByTagName("html")[0];
+
+const lightModeButton = document.getElementById("light-mode");
+const darkModeButton = document.getElementById("dark-mode");
+
+lightModeButton.addEventListener("click", lightMode);
+darkModeButton.addEventListener("click", darkMode);
+
+//Working out if Operating System is in light mode or dark mode
+if (
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches
+) {
+  console.log("dark mode");
+  darkMode();
+} else {
+  lightMode();
+}
+
+function lightMode() {
+  console.log("light mode");
+  html.style.setProperty("--text-color", "black");
+  html.style.setProperty("--background-color", "#f1f1f1");
+  html.style.setProperty("--white", "white");
+  html.style.setProperty("--nav-button", "rgba(0, 0, 0, 0.6)");
+  html.style.setProperty("--nav-color", "rgba(255, 255, 255, 0.9)");
+  html.style.setProperty("--font-weight", "bold");
+  html.style.setProperty("--background-gradient", "rgba(255, 255, 255, 0.4)");
+  html.style.setProperty("--card-background", "#f1f1f1");
+  html.style.setProperty("--projects-shadow", "#424242");
+  html.style.setProperty("--card-bar-empty", "lightgray");
+  html.style.setProperty("--card-bar-full", "grey");
+  html.style.setProperty("--card-icons", "lightgrey");
+  html.style.setProperty(
+    "--background-gradient-contact",
+    "rgba(255, 255, 255, 0.2)"
+  );
+  closeNav();
+}
+
+function darkMode() {
+  console.log("dark mode");
+  html.style.setProperty("--text-color", "#f1f1f1");
+  html.style.setProperty("--background-color", "#1b1b1b");
+  html.style.setProperty("--white", "black");
+  html.style.setProperty("--nav-button", "rgba(255, 255, 255, 0.6)");
+  html.style.setProperty("--nav-color", "rgba(0, 0, 0, 0.85)");
+  html.style.setProperty("--font-weight", "normal");
+  html.style.setProperty("--background-gradient", "rgba(0, 0, 0, 0.87)");
+  html.style.setProperty("--card-background", "#2d2d2e");
+  html.style.setProperty("--projects-shadow", "#1a1110");
+  html.style.setProperty("--card-bar-empty", "#444");
+  html.style.setProperty("--card-bar-full", "grey");
+  html.style.setProperty("--card-icons", "#444");
+  html.style.setProperty(
+    "--background-gradient-contact",
+    "rgba(0, 0, 0, 0.65)"
+  );
+  closeNav();
+}
