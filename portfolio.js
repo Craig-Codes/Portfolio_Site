f8fad51cf82583b5daf96b9748d1ad67c4d840ad
CraigA1987 <craigadam1987@gmail.com>
2020-02-23 00:50:44 +0000

    Added a Preloader

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,17 @@
+// Preloader
+$("body").append(
+  '<div style="" id="loadingDiv"><img class="loader" src="/imgs/preloader.gif" alt="Loading..."></div>'
+);
+$(window).on("load", function() {
+  setTimeout(removeLoader, 500); //wait for page load PLUS half a second.
+});
+function removeLoader() {
+  $("#loadingDiv").fadeOut(500, function() {
+    // fadeOut complete. Remove the loading div
+    $("#loadingDiv").remove(); //makes page more lightweight
+  });
+}
+
 let clicked = false;
 
 const openNavButton = document.querySelector(".nav-expand-button");
