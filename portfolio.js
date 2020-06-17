9d57a9201d867df6b946ad4547c676d4d547e567
CraigA1987 <48647012+CraigA1987@users.noreply.github.com>
2020-06-17 14:28:08 +0100

    Add IntersectObserver

--- portfolio.js
+++ portfolio.js
@@ -1,11 +1,14 @@
-// Preloader - remove on page load complete
-$(window).on("load", function () {
+// Pre-Loader Code
+window.addEventListener("load", () => {
   setTimeout(removeLoader, 500);
 });
+
 function removeLoader() {
-  $("#loadingDiv").fadeOut(500, function () {
-    $("#loadingDiv").remove();
-  });
+  const x = document.getElementById("loadingDiv");
+  x.style.opacity = "0";
+  setTimeout(() => {
+    x.remove();
+  }, 1000);
 }
 
 // Navbar
@@ -44,46 +47,76 @@ introDown.addEventListener("click", function () {
   location.href = "#about-page";
 });
 
-// Code to deal with knowing when a div is in the viewport to trigger a CSS Keyframe animation. Uses the jquery library to help out.
-function isOnScreen(element) {
-  var curPos = element.offset();
-  var curTop = curPos.top;
-  var screenHeight = $(window).height();
-  return curTop > screenHeight ? false : true;
-}
+// // Code to deal with knowing when a div is in the viewport to trigger a CSS Keyframe animation. Uses the jquery library to help out.
+// function isOnScreen(element) {
+//   var curPos = element.offset();
+//   var curTop = curPos.top;
+//   var screenHeight = $(window).height();
+//   return curTop > screenHeight ? false : true;
+// }
 
-// Functions trigger when certain elements appear in the viewport - Interval so that function is constantly looking
-setInterval(animateOnView, 500);
+// // Functions trigger when certain elements appear in the viewport - Interval so that function is constantly looking
+// setInterval(animateOnView, 500);
 
-function animateOnView() {
-  if (isOnScreen($("#about-underline"))) {
-    $("#about-underline").animate(
-      {
-        width: "100%"
-      },
-      "slow"
-    );
-  }
-  if (isOnScreen($("#project-underline"))) {
-    $("#project-underline").animate(
-      {
-        width: "100%"
-      },
-      "slow"
-    );
-  }
-  if (isOnScreen($("#tech-underline"))) {
-    $("#tech-underline").animate(
-      {
-        width: "100%"
-      },
-      "slow"
-    );
-  }
-  // if (isOnScreen($("#about_image"))) {
-  //   $("#columnTwo img").animate({ width: "100%" }, "slow");
-  // }
-}
+// function animateOnView() {
+//   if (isOnScreen($("#about-underline"))) {
+//     $("#about-underline").animate(
+//       {
+//         width: "100%",
+//       },
+//       "slow"
+//     );
+//   }
+//   if (isOnScreen($("#project-underline"))) {
+//     $("#project-underline").animate(
+//       {
+//         width: "100%",
+//       },
+//       "slow"
+//     );
+//   }
+//   if (isOnScreen($("#tech-underline"))) {
+//     $("#tech-underline").animate(
+//       {
+//         width: "100%",
+//       },
+//       "slow"
+//     );
+//   }
+//   // if (isOnScreen($("#about_image"))) {
+//   //   $("#columnTwo img").animate({ width: "100%" }, "slow");
+//   // }
+// }
+
+const options = {
+  root: null, // use the document's viewport as the container
+  rootMargin: "0px", // % or px - offsets added to each side of the intersection
+  threshold: 0.2, // percentage of the target element which is visible
+};
+
+let callback = (entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    // If entry (box) is visible - according with the params set in `options`
+    // then adds `isVisible` class to box
+    // otherwise removes `isVisible` class
+    if (entry.isIntersecting) {
+      entry.target.classList.add("isVisible");
+    } else {
+      entry.target.classList.remove("isVisible");
+    }
+  });
+};
+
+// Create the intersection observer instance by calling its constructor and passing it a
+// callback function to be run whenever a threshold is crossed in one direction or the other:
+let observer = new IntersectionObserver(callback, options);
+
+// Get all the `.box` from DOM and attach the observer to these
+document.querySelectorAll(".observed").forEach((element) => {
+  observer.observe(element);
+});
+
 
 // Dealing with theme change between light and dark
 const html = document.getElementsByTagName("html")[0];
@@ -110,7 +143,6 @@ function lightMode() {
   html.style.setProperty("--white", "white");
   html.style.setProperty("--nav-button", "rgba(0, 0, 0, 0.6)");
   html.style.setProperty("--nav-color", "rgba(255, 255, 255, 0.9)");
-  //html.style.setProperty("--font-weight", "bold");
   html.style.setProperty("--background-gradient", "rgba(255, 255, 255, 0.4)");
   html.style.setProperty("--card-background", "#f1f1f1");
   html.style.setProperty("--projects-shadow", "#424242");
@@ -123,9 +155,7 @@ function lightMode() {
   );
   html.style.setProperty("--footer-border", "black");
   closeNav();
-  particlesJS.load("particles-js", "/assets/particles.json", function () {
-  });
-  //document.getElementById("ionic").src = "./imgs/ionic_black.png";
+  particlesJS.load("particles-js", "/assets/particles.json");
 }
 
 function darkMode() {
@@ -134,7 +164,6 @@ function darkMode() {
   html.style.setProperty("--white", "black");
   html.style.setProperty("--nav-button", "rgba(255, 255, 255, 0.6)");
   html.style.setProperty("--nav-color", "rgba(0, 0, 0, 0.85)");
-  // html.style.setProperty("--font-weight", "normal");
   html.style.setProperty("--background-gradient", "rgba(0, 0, 0, 0.87)");
   html.style.setProperty("--card-background", "#2d2d2e");
   html.style.setProperty("--projects-shadow", "#1a1110");
@@ -147,7 +176,5 @@ function darkMode() {
   );
   html.style.setProperty("--footer-border", "white");
   closeNav();
-  particlesJS.load("particles-js", "/assets/particles_dark.json", function () {
-  });
- // document.getElementById("ionic").src = "./imgs/ionic_white.png";
+  particlesJS.load("particles-js", "/assets/particles_dark.json");
 }
